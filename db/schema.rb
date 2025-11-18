2b6b562c3500decd716fd448cd2b95a405e2d841
mightysosuke <sou32suke@gmail.com>
2025-11-18 19:45:08 +0900

    Photoのマイグレーションファイルおよびモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_18_092831) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_18_104231) do
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
@@ -28,5 +36,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_18_092831) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "photos", "users"
   add_foreign_key "sessions", "users"
 end
