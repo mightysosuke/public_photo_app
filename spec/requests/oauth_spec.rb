7a77b18ecd16dc423533bb1991cd948a820fb71c
mightysosuke <sou32suke@gmail.com>
2025-11-23 21:47:09 +0900

    GET /oauth/callbackのリクエストスペック実装

--- spec/requests/oauth_spec.rb
+++ spec/requests/oauth_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe "Oauth", type: :request do
       expect(response).to have_http_status(:redirect)
     end
   end
+
+  describe "GET /oauth/callback" do
+    let(:mock_response) { instance_double(Net::HTTPResponse) }
+    let(:token_response) { { access_token: "test_access_token" }.to_json }
+
+    before do
+      allow(Net::HTTP).to receive(:post_form).and_return(mock_response)
+      allow(mock_response).to receive(:body).and_return(token_response)
+    end
+
+    context "認可コードが渡された場合" do
+      it "photos_pathにリダイレクトすること" do
+        get oauth_callback_path, params: { code: "test_code" }
+        expect(response).to redirect_to(photos_path)
+      end
+    end
+  end
 end
