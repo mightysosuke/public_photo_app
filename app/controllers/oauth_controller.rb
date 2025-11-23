0691ca94eb43033d18bca3090ad2ff0c092b30f6
mightysosuke <sou32suke@gmail.com>
2025-11-23 18:30:08 +0900

    OAuthアクセストークンを取得してセッションに保存

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,4 +1,8 @@
 class OauthController < ApplicationController
+  require "net/http"
+  require "uri"
+  require "json"
+
   def authorize
     authorize_url = "#{Rails.application.credentials.twitter[:url]}/oauth/authorize"
 
@@ -13,7 +17,20 @@ class OauthController < ApplicationController
   end
 
   def callback
-    # コールバック後の処理は後ほど実装、ひとまず写真一覧にリダイレクト
+    code = params[:code]
+
+    uri = URI("#{Rails.application.credentials.twitter[:url]}/oauth/token")
+    token_params = {
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: "http://localhost:3000/oauth/callback",
+      client_id: Rails.application.credentials.twitter[:client_id],
+      client_secret: Rails.application.credentials.twitter[:client_secret]
+    }
+    response = Net::HTTP.post_form(uri, token_params)
+    token_response = JSON.parse(response.body)
+
+    session[:twitter_access_token] = token_response["access_token"]
     redirect_to photos_path
   end
 end
