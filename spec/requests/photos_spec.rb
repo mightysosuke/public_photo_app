869fb1a30a2a6d5eaf031b94e077d299f63b05bf
mightysosuke <sou32suke@gmail.com>
2025-11-23 21:31:36 +0900

    GET /photosのリクエストスペック実装

---
+++ spec/requests/photos_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Photos", type: :request do
+  let(:user) { create(:user) }
+
+  before do
+    post session_path, params: { email_address: user.email_address, password: "password" }
+  end
+
+  describe "GET /photos" do
+    context "リクエストを送った場合" do
+      it "successのステータスが返ってくること" do
+        get photos_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
